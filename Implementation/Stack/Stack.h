6e648dd0dc672d0820aec5452e4d9ca20d847980
Educpf <eduardocportugalf@gmail.com>
2023-09-01 23:08:08 +0100

    Created the implementation of a new data structure. Stacks. Created header file and also test file. Joined aswell the makefile

---
+++ Implementation/Stack/Stack.h
@@ -0,0 +1,94 @@
+
+
+#ifndef Stack_H
+#define Stack_H
+
+#include <initializer_list>
+#include <stdexcept>
+#include <iostream>
+
+template <typename T>
+class Pure_Stack{
+
+    private:
+        
+        struct Node {
+            T data;
+            Node* next; 
+        };
+
+    private:
+
+        Node* top;
+        size_t tam;
+
+    public:
+
+        Pure_Stack(): top(nullptr), tam(0){
+            std::cout << "Stack created!\n"; 
+        };
+        Pure_Stack(const std::initializer_list<T>& list):top(nullptr), tam(list.size){
+            if (!empty()){
+                for (const T& value : list){
+                    push_back(value);
+                }
+            }
+        }
+        ~Pure_Stack(){
+            std::cout << "Destructor called! On size " << tam << "\n";
+            while (!empty()){
+                pop_back();
+            }
+        }
+
+        void push_back(T value) {
+            tam++;
+            Node* temp= top;
+            top = new Node;
+            top->data = value;
+            top->next = temp;
+            std::cout << "added value: " << value << "\n";
+        }
+        T pop_back(){
+            if (!empty()){
+                tam--;
+                T value = top->data;
+                Node* del = top;
+                top = top->next;
+                delete del;
+                std::cout << "Poped " << value << "\n"; 
+                return value;
+            } else throw std::length_error("Stack already empty");
+        }
+        T back(){
+            if (tam > 0) return top->data;
+            else throw std::length_error("Stack already empty");
+        }
+        size_t size(){
+            return tam;
+        }
+        bool empty(){
+            return (tam == 0);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+#endif
---
+++ Implementation/Stack/test.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+#include "Stack.h"
+
+int main(){
+
+
+    Pure_Stack<int> s;
+
+    s.push_back(1);
+    s.push_back(3);
+    s.push_back(6);
+    s.push_back(8);
+    std::cout << s.back() << '\n';
+
+
+
+
+
+    return 0;
+}
